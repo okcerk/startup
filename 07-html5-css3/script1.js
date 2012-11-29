03a7b24863f383d3448a4ab5addb0d3d3629a473
Andrés Marc <andresmarconato@gmail.com>
2012-11-29 15:50:37 +0100

    v1

---
+++ 07-html5-css3/script1.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$("#menu").mouseover(function(){
+		$("ul#menu").css("font-size","25px");
+	});
+	$("#menu").mouseout(function(){
+		$("ul#menu").css("font-size","15px");
+	});
+$(window).resize(function()
+{
+	var art2width = $("#article2").width();
+	if (art2width<900){
+		$("#article2").css("float", "left");
+	}
+	else{
+		$("#article2").css("float", "");
+	}
+});
+});
